384fddcb06669e94812aa7c1065595653f6cab7a
nataliafatkina <fatkina.natali@gmail.com>
2024-09-07 12:08:50 +0300

    Add files via upload

--- module_2_6.py
+++ module_2_6.py
@@ -1,8 +1,8 @@
 n = int(input("Введите число от 3 до 20: "))
 result = []
 
-for i in range(1, n):
-    for j in range(i+1, n):
+for i in range(1, 21):
+    for j in range(i+1, 21):
         if (i + j) % n == 0 or n % (i + j) == 0:
             result.append(i)
             result.append(j)
